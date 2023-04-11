7747d9bb9d3fe95dbdee498c84b37a8cf7ac27b0
LisetteRinnen <77075034+LisetteRinnen@users.noreply.github.com>
2023-04-10 23:35:26 -0700

    Update TCPClient.java

--- TCPClient.java
+++ TCPClient.java
@@ -7,12 +7,26 @@ public class TCPClient {
     String serverUrl = args[0];
     int port = Integer.valueOf(args[1]);
 
+
     try (Socket socket = new Socket(serverUrl, port)) {
-      // Socket socket = new Socket(serverUrl, port); // Original?
+
+      StringBuilder message = new StringBuilder();
+      int numArgs = args.length;
+
+      // check to see if there is a message
+      for (int i = 2; i < numArgs; i++) {
+        message.append(args[i] + ' ');
+      }
+      message.append("\n");
+
+      // send message to server
+      OutputStream output = socket.getOutputStream();
+      output.write(message.toString().getBytes());
+
+      // get response from server
       InputStream input = socket.getInputStream();
 
       int exists = input.read();
-      // if input.read() == -1, end of stream
       while (exists != -1) {
         System.out.write(exists);
         exists = input.read();
@@ -23,6 +37,5 @@ public class TCPClient {
       // if any errors occur with socket connection
       e.printStackTrace();
     }
-
   }
 }
